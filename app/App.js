ed6c18daffb1835b07bb9424b793016067c2428c
Gerardo Arevalo <gerardo@atctech.co>
2016-07-22 17:35:39 -0400

    added react router, hashHistory warning fix

--- app/App.js
+++ app/App.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import styles from './App.css';
 import { render } from 'react-dom';
 
+import { Router, Route, Link } from 'react-router';
+
 import About from './About';
 import Home from './Home';
 import Repos from './Repos';
@@ -9,39 +11,21 @@ import Repos from './Repos';
 class App extends Component {
   constructor() {
     super(...arguments);
-    this.state = {
-      route: window.location.hash.substr(1)
-    };
-  }
-
-  componentDidMount() {
-    window.addEventListener('hashchange', () => {
-      this.setState({
-        route: window.location.hash.substr(1)
-      });
-    });
   }
 
   render() {
-    var Child;
-    switch (this.state.route) {
-      case '/about': Child = About; break;
-      case '/repos': Child = Repos; break;
-      default: Child = Home;
-    }
-    
     return (
       <div>
         <header>App</header>
         <menu>
           <ul>
-            <li><a href="#/about">About</a></li>
-            <li><a href="#/repos">Repos</a></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/repos">Repos</Link></li>
           </ul>
         </menu>
-        <Child/>
+        {this.props.children}
       </div>
-    )
+    );
   }
 }
 
--- app/main.js
+++ app/main.js
@@ -3,5 +3,18 @@ import {render} from 'react-dom';
 import App from './App';
 import './main.css';
 
+import About from './About';
+import Home from './Home';
+import Repos from './Repos';
 
-render(<App />, document.getElementById('root'));
+import { Router, Route, Link } from 'react-router';
+import { hashHistory } from 'react-router'
+
+render((
+    <Router history={hashHistory}>
+        <Route path="/" component={App}>
+            <Route path="about" component={About}/>
+            <Route path="repos" component={Repos}/>
+        </Route>
+    </Router>
+), document.getElementById('root'));
