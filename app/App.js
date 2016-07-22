be90039f75acb896f1efec17eab3cd6f7e59ce39
Gerardo Arevalo <gerardo@atctech.co>
2016-07-22 16:59:08 -0400

    really simple routing

--- app/App.js
+++ app/App.js
@@ -1,13 +1,47 @@
 import React, {Component} from 'react';
 import styles from './App.css';
+import { render } from 'react-dom';
 
-class App extends Component{
+import About from './About';
+import Home from './Home';
+import Repos from './Repos';
+
+class App extends Component {
+  constructor() {
+    super(...arguments);
+    this.state = {
+      route: window.location.hash.substr(1)
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener('hashchange', () => {
+      this.setState({
+        route: window.location.hash.substr(1)
+      });
+    });
+  }
+
   render() {
+    var Child;
+    switch (this.state.route) {
+      case '/about': Child = About; break;
+      case '/repos': Child = Repos; break;
+      default: Child = Home;
+    }
+    
     return (
-      <div className={styles.root}>
-        <h1>Hello World</h1>
+      <div>
+        <header>App</header>
+        <menu>
+          <ul>
+            <li><a href="#/about">About</a></li>
+            <li><a href="#/repos">Repos</a></li>
+          </ul>
+        </menu>
+        <Child/>
       </div>
-    );
+    )
   }
 }
 
